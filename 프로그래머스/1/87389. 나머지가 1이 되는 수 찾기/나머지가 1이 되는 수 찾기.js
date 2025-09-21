12a4aefeca26c0702badcdef8d3b516415ccc10a
suki186 <100691648+suki186@users.noreply.github.com>
2025-09-21 14:07:12 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -1,3 +1,4 @@
+// 내가 푼 코드: 매우 효율적이지만 길다. 실무/대규모 입력 O(√n)
 const getSmallest = (n) => {
     // 2부터 시작
     for (let i = 2; i <= Math.sqrt(n); i++) {
@@ -16,3 +17,14 @@ function solution(n) {
     
     return answer;
 }
+
+// 단순 구현엔 좋지만 n이 커지면 오래 걸림 O(n)
+/*
+function solution(n) {
+    var answer = 0;
+    
+    for(var i = 2; i < n; i++) {
+        if (n % i === 1) return i;
+    }
+}
+*/
