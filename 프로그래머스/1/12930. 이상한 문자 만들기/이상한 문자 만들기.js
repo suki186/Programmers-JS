9994b30c56bf55b9ccebc8ca9898dd0536e9ebcf
suki186 <100691648+suki186@users.noreply.github.com>
2025-10-03 01:59:54 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,16 @@
+function solution(s) {
+    var answer = '';
+    
+    // 공백 기준 분리
+    const arr = s.split(" ");
+
+    // 반복문 -> 짝수 대문자, 홀수 소문자
+    arr.forEach((e, idx) => {
+        for (let i = 0; i < e.length; i++) {
+            answer += i % 2 === 0 ? e[i].toUpperCase() : e[i].toLowerCase();
+        }
+        if (idx < arr.length - 1) answer += " "; // 공백 추가
+    })
+    
+    return answer;
+}
