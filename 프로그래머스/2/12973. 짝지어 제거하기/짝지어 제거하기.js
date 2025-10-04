5c95b817ac755a3049285679f286a6e4c47676f1
suki186 <100691648+suki186@users.noreply.github.com>
2025-10-04 21:19:52 +0900

    [level 2] Title: 짝지어 제거하기, Time: 45.49 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -2,9 +2,15 @@ function solution(s)
 {
     const stack = []; // 스택
     
+    /*
     for(const e of s) {
         if(e === stack[stack.length-1]) stack.pop();
         else stack.push(e);
+    }
+    */
+    for (let i = 0; i < s.length; i++) {
+        if(s[i] === stack[stack.length-1]) stack.pop();
+        else stack.push(s[i]);
     }
 
     return stack.length === 0 ? 1 : 0;
