77007bc19631dafd985db6ee19c8eb6d327bee9e
suki186 <100691648+suki186@users.noreply.github.com>
2025-10-07 00:39:21 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -2,7 +2,8 @@ function solution(n) {
     var answer = 0;
 
     // 3진수 -> 문자열 -> 배열 -> 앞뒤반전 -> 합치기
-    n = String(n.toString(3)).split('').reverse().join('');
+    //n = String(n.toString(3)).split('').reverse().join('');
+    n = [...n.toString(3)].reverse().join("");
     answer = parseInt(n, 3); // 3진법 -> 10진법
     
     return answer;
