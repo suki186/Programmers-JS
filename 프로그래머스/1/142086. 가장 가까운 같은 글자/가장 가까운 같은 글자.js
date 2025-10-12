f18e26843abdd69128a26a2d73aaac1dc24e2afc
suki186 <100691648+suki186@users.noreply.github.com>
2025-10-12 15:00:09 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 36.78 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -5,10 +5,8 @@ function solution(s) {
     for (let i = 1; i < s.length; i++) {
         const last = words.lastIndexOf(s[i]); // 가장 최근의 같은 글자
         
-        // 처음 나온 문자
-        if (last === -1) answer.push(-1);
-        // 이미 나온적 있는 문자
-        else answer.push(i - last);
+        // 이미 나온적이 있다면 i-last
+        last === -1 ? answer.push(-1) : answer.push(i-last);
         
         words.push(s[i]);
     }
