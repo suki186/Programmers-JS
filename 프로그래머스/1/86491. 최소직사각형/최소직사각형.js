9c1a47dbe9abd7d933a357abe769bfec61c4c646
suki186 <100691648+suki186@users.noreply.github.com>
2025-10-12 15:23:25 +0900

    [level 1] Title: 최소직사각형, Time: 6.07 ms, Memory: 40.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,4 +1,4 @@
-function solution(sizes) {
+function solution2(sizes) {
     let x = 0, y = 0;
     let maxX = 0, maxY = 0; // 최종 가로, 세로
     
@@ -11,4 +11,17 @@ function solution(sizes) {
         maxY = Math.max(maxY, y); // 지금까지 가장 큰 세로
     }
     return maxX * maxY;
+}
+
+// map 함수 사용
+function solution(sizes) {
+    // 큰수가 앞으로
+    const rotated = sizes.map(([w, h]) => w < h ? [h, w] : [w, h]);
+    let maxSize = [0, 0];
+    
+    rotated.forEach(([w, h]) => {
+        if (w > maxSize[0]) maxSize[0] = w;
+        if (h > maxSize[1]) maxSize[1] = h;
+    })
+    return maxSize[0]*maxSize[1];
 }
